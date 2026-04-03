067c538800355145113008359d4eb919f4b125a3
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2026-04-03 21:26:41 +0530

    update

--- examples/leWorldModel/lewm_loader/__init__.py
+++ examples/leWorldModel/lewm_loader/__init__.py
@@ -1,9 +1,8 @@
-from .dataset import LeWMLanceDataset, compute_normalizers
+from .dataset import LeWMLanceDataset
 from .dataloaders import make_lewm_lance_loader, make_train_val_loaders
 
 __all__ = [
     "LeWMLanceDataset",
-    "compute_normalizers",
     "make_lewm_lance_loader",
     "make_train_val_loaders",
 ]
